eb3889abaa5686710c959a47bb7237e66d0c6415
AhmS3cOps <alkdeydocyber@gmail.com>
2025-04-04 16:23:05 +0000

    Project Done.

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def crackHash(inputPass):
         encPass = password.encode("utf-8")
         digest = hashlib.md5(encPass.strip()).hexdigest()
 
+
         if digest == inputPass:
             print("Password Found: " + password)
 
